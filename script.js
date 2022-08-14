3978ec3d6080bdc492163031fa528dcf76e47e87
ErikNWH <erik.nwh@gmail.com>
2022-08-14 16:25:46 +0200

    fully working

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function darkMode() {
 function lightMode() {
   nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
   textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-  toggleIcon.children[0].textContent = 'Dark Mode';
+  toggleIcon.children[0].textContent = 'Light Mode';
   toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
   imageMode('light');
 }
@@ -35,13 +35,26 @@ function lightMode() {
 function switchTheme(event) {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
     darkMode();
   } else {
     document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
     lightMode();
   }
 }
 
 // Event Listener
-
 toggleSwitch.addEventListener('change', switchTheme);
+
+// Check local Storage
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+    darkMode();
+  }
+}
